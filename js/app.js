7484bdc9aa6b17e9aa4c6fa41d8a818e6836c50d
ivan-martynov <martinwolf@MacBook.Dlink>
2015-09-16 18:28:34 +0300

    init

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/*global jQuery, Handlebars, Router */
+jQuery(function ($) {
+	'use strict';
+
+	Handlebars.registerHelper('eq', function (a, b, options) {
+		return a === b ? options.fn(this) : options.inverse(this);
+	});
+
+	var ENTER_KEY = 13;
+	var ESCAPE_KEY = 27;
+}
